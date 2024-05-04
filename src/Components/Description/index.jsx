db7705c9247a8b82d5cea0d97bfce2af40ea6128
vasant.jk <vasant.jk@dazn.com>
2024-05-04 17:20:41 +0530

    show more and show less moved to modal

--- src/Components/Description/index.jsx
+++ src/Components/Description/index.jsx
@@ -1,20 +1,39 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+
 import './description.css';
 
 export default function Description({ jobDetailsFromCompany }) {
   const [toggle, setToggle] = useState(false);
 
+  const handleClickOpen = () => {
+    setToggle(true);
+  };
+  const handleClose = () => {
+    setToggle(false);
+  };
+
   return (
     <div className='desc-wrapper'>
-      <p className='desc' data-expand={toggle}>
-        {jobDetailsFromCompany}
-      </p>
+      <p className='desc'>{jobDetailsFromCompany}</p>
 
-      <button onClick={() => setToggle((prev) => !prev)}>
+      <button onClick={handleClickOpen}>
         {toggle ? 'show less' : 'show more'}
       </button>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby='customized-dialog-title'
+        open={toggle}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }}>About Company</DialogTitle>
+        <DialogContent sx={{ m: 0, p: 2 }}>
+          {jobDetailsFromCompany}
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
