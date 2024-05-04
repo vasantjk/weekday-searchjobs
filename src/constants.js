449bbb33702fe3c34a90332d4965879086d50bf9
vasant.jk <vasant.jk@dazn.com>
2024-05-04 12:57:52 +0530

    drop down value are moved to seperate file constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const ROLES = ['ios', 'frontend', 'backend', 'android', 'tech lead'];
+const EXPERIENCE = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const MIN_BASE_PAY = ['10L', '20L', '30L', '40L', '50L', '60L', '70L'];
+const REMOTE_OPTIONS = [
+  'remote',
+  'hybrid',
+  'in-office',
+  'mumbai',
+  'chennai',
+  'bangalore',
+  'delhi ncr',
+];
+
+export { ROLES, EXPERIENCE, MIN_BASE_PAY, REMOTE_OPTIONS };
